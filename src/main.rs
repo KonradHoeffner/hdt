53b921d59f8c9b8a4cbd8be8416bfb38324dd510
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2025-04-10 21:32:25 +0200

    switch from argh to clap and improve error output

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-//! *This module is available only if HDT is built with the `"sophia"` feature.*
-//! Under development, parameters may change.
 #[cfg(feature = "sophia")]
-use argh::FromArgs;
-use hdt::{Hdt, HdtGraph};
+#[cfg(feature = "cli")]
+/// *This module is available only if HDT is built with the `"sophia"` feature.*
+/// Under development, parameters may change.
+use clap::Parser;
+use color_eyre::eyre::{Report, WrapErr};
+use hdt::Hdt;
 use log::info;
 use sophia::api::prelude::{Stringifier, TripleSerializer};
 use sophia::turtle::serializer::nt::NtSerializer;
@@ -16,60 +18,62 @@ use std::io::{BufReader, stdin};
     Turtle,
 }*/
 
-#[derive(FromArgs)]
 /// convert HDT to N-Triples
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
 struct Args {
-    // #[argh(option, short = 'f')]
     // /// RDF Format of the output
     //format: Format,
-    #[argh(switch, short = 't')]
+    #[arg(short, long, default_value_t = false)]
     /// export as RDF Turtle, default is N-Triples
     turtle: bool,
 
     // /// verbose output
-    // #[argh(switch, short = 'v')]
     //verbose: bool,
-    /// the file to load, if not given it is read from stdin
-    #[argh(positional)]
-    filename: Option<String>,
+    /// the HDT file to load from, if not given it is read from stdin
+    hdt_input_file: Option<String>,
+    /// the RDF file to create, if not given it is written to stdout
+    rdf_output_file: Option<String>,
 }
 
-fn main() {
+fn main() -> Result<(), Report> {
     color_eyre::install().unwrap();
     //env_logger::init();
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
-    let args: Args = argh::from_env();
-    let hdt = match args.filename {
+    //env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+    let args = Args::parse();
+    let hdt = match args.hdt_input_file {
         Some(filename) => {
-            let file = File::open(filename.clone()).expect("error opening file");
-            let hdt = Hdt::new(std::io::BufReader::new(file)).expect("Error loading HDT from {filename}");
-            info!("Loaded from file {filename} {hdt:?}");
+            let file =
+                File::open(filename.clone()).wrap_err_with(|| format!("Error opening input file {}", filename))?;
+            let hdt = Hdt::read(std::io::BufReader::new(file))
+                .wrap_err_with(|| format!("Error loading HDT from {}", filename))?;
+            //info!("Loaded from file {filename} {hdt:?}");
             hdt
         }
         None => {
             let reader = BufReader::new(stdin());
-            let hdt = Hdt::new(reader).expect("Error loading HDT from standard input");
+            let hdt = Hdt::read(reader).wrap_err("Error loading HDT from standard input")?;
             info!("Loaded from stdin {hdt:?}");
             hdt
         }
     };
-    let graph = HdtGraph::new(hdt);
     let s = match args.turtle {
         true => {
             let config = TurtleConfig::new().with_pretty(true);
             //.with_own_prefix_map(prefixes().clone());
             TurtleSerializer::new_stringifier_with_config(config)
-                .serialize_graph(&graph)
-                .expect("error serializing graph as RDF Turtle")
+                .serialize_graph(&hdt)
+                .wrap_err("error serializing graph as RDF Turtle")?
                 .to_string()
         }
         false => {
             // Default: export the complete graph as N-Triples.
             NtSerializer::new_stringifier()
-                .serialize_graph(&graph)
-                .expect("error serializing graph as N-Triples")
+                .serialize_graph(&hdt)
+                .wrap_err("error serializing graph as N-Triples")?
                 .to_string()
         }
     };
     println!("{s}");
+    Ok(())
 }
