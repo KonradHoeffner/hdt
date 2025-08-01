539bce6a92056fd8a33497ae88eaff587b35ab9a
Greg Hanson <greg.hanson@decisym.ai>
2025-08-01 07:00:34 -0400

    first pass implementing oxigraph spareval query (#66)
    ---------
    Co-authored-by: Konrad Höffner <konrad.hoeffner@uni-leipzig.de>

--- src/lib.rs
+++ src/lib.rs
@@ -112,6 +112,8 @@ use containers::ControlInfo;
 use dict_sect_pfc::DictSectPFC;
 use four_sect_dict::FourSectDict;
 pub use four_sect_dict::IdKind;
+#[cfg(feature = "sparql")]
+pub mod sparql;
 
 #[cfg(test)]
 mod tests {
