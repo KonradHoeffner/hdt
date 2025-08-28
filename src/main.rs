30293723c31fa2d30421789a4cd351530d33972c
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2025-08-28 16:08:42 +0200

    add CLI commands

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 /// *This module is available only if HDT is built with the `"sophia"` feature.*
 /// Under development, parameters may change.
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use color_eyre::config::HookBuilder;
 use color_eyre::eyre::{Report, WrapErr};
-use hdt::Hdt;
+use hdt::{Hdt, containers::ControlInfo, header::Header};
 //use log::info;
 use fs_err::File;
 use sophia::api::prelude::{Stringifier, TripleSerializer};
 use sophia::turtle::serializer::nt::NtSerializer;
 use sophia::turtle::serializer::turtle::{TurtleConfig, TurtleSerializer};
+use std::io::BufReader;
 //use std::io::{BufReader, stdin};
 
 /*enum Format {
@@ -18,26 +19,38 @@ use sophia::turtle::serializer::turtle::{TurtleConfig, TurtleSerializer};
 }*/
 
 /// convert HDT to N-Triples
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
+    #[command(subcommand)]
+    command: Command,
     // /// RDF Format of the output
     //format: Format,
-    #[arg(short, long)]
-    /// export as RDF Turtle, default is N-Triples
-    turtle: bool,
-
-    #[arg(short, long)]
-    /// Count triples only, do not print them
-    count: bool,
 
     // /// verbose output
     //verbose: bool,
     // disable std reading for now because of usability downside for new users when started with no parameter // the HDT file to load from, if not given it is read from stdin
-    /// the HDT file to load from
-    hdt_input_file: String,
-    /// the RDF file to create, if not given it is written to stdout
-    rdf_output_file: Option<String>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Info {
+        hdt_input_file: String,
+    },
+    Convert {
+        #[arg(short, long)]
+        /// export as RDF Turtle, default is N-Triples
+        turtle: bool,
+        /*
+        #[arg(short, long)]
+        /// Count triples only, do not print them
+        count: bool,
+        */
+        /// the HDT file to load from
+        hdt_input_file: String,
+        /// the RDF file to create, if not given it is written to stdout
+        rdf_output_file: Option<String>,
+    },
 }
 
 fn main() -> Result<(), Report> {
@@ -49,7 +62,7 @@ fn main() -> Result<(), Report> {
         Some(filename) => {
             let file =
                 File::open(filename.clone()).wrap_err_with(|| format!("Error opening input file {}", filename))?;
-            Hdt::read(std::io::BufReader::new(file))
+            Hdt::read(BufReader::new(file))
                 .wrap_err_with(|| format!("Error loading HDT from {}", filename))?
             //info!("Loaded from file {filename} {hdt:?}");
         }
@@ -59,32 +72,44 @@ fn main() -> Result<(), Report> {
             //info!("Loaded from stdin {hdt:?}");
         }
     };*/
-    let filename = args.hdt_input_file;
-    let file = File::open(filename.clone()).with_context(|| format!("Error opening input HDT file {filename}"))?;
-    let hdt = Hdt::read(std::io::BufReader::new(file))
-        .with_context(|| format!("Error loading input HDT from {}", filename))?;
-    let count = hdt.triples.len();
-    if args.count {
-        println!("Parsing returned {} triples", count);
-        return Ok(());
+    match args.command {
+        Command::Info { hdt_input_file } => {
+            let file = File::open(hdt_input_file.clone())
+                .with_context(|| format!("Error opening input HDT file {hdt_input_file}"))?;
+            let mut reader = BufReader::new(file);
+            ControlInfo::read(&mut reader)?;
+            let header = Header::read(&mut reader)?;
+            println!("HDT File: {:#?}", header.body);
+        }
+        Command::Convert { hdt_input_file, rdf_output_file, turtle } => {
+            let file = File::open(hdt_input_file.clone())
+                .with_context(|| format!("Error opening input HDT file {hdt_input_file}"))?;
+            let hdt = Hdt::read(BufReader::new(file))
+                .with_context(|| format!("Error loading input HDT from {hdt_input_file}"))?;
+            let count = hdt.triples.len();
+            /*if args.count {
+                println!("Parsing returned {} triples", count);
+                return Ok(());
+            }*/
+            let s = match turtle {
+                true => {
+                    let config = TurtleConfig::new().with_pretty(true);
+                    //.with_own_prefix_map(prefixes().clone());
+                    TurtleSerializer::new_stringifier_with_config(config)
+                        .serialize_graph(&hdt)
+                        .wrap_err("error serializing graph as RDF Turtle")?
+                        .to_string()
+                }
+                false => {
+                    // Default: export the complete graph as N-Triples.
+                    NtSerializer::new_stringifier()
+                        .serialize_graph(&hdt)
+                        .wrap_err("error serializing graph as N-Triples")?
+                        .to_string()
+                }
+            };
+            println!("{s}");
+        }
     }
-    let s = match args.turtle {
-        true => {
-            let config = TurtleConfig::new().with_pretty(true);
-            //.with_own_prefix_map(prefixes().clone());
-            TurtleSerializer::new_stringifier_with_config(config)
-                .serialize_graph(&hdt)
-                .wrap_err("error serializing graph as RDF Turtle")?
-                .to_string()
-        }
-        false => {
-            // Default: export the complete graph as N-Triples.
-            NtSerializer::new_stringifier()
-                .serialize_graph(&hdt)
-                .wrap_err("error serializing graph as N-Triples")?
-                .to_string()
-        }
-    };
-    println!("{s}");
     Ok(())
 }
