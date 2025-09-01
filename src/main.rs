d73365fa69b7f5bc34477b37c4e0bab6ef073730
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2025-09-01 12:13:01 +0200

    get conversion from and to HDT to work

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,16 @@ use color_eyre::eyre::{Report, WrapErr};
 use hdt::{Hdt, containers::ControlInfo, header::Header};
 //use log::info;
 use fs_err::File;
-use sophia::api::prelude::{Stringifier, TripleSerializer};
+use sophia::api::graph::Graph;
+use sophia::api::prelude::{TripleSerializer, TripleSource};
+//use sophia::api::prelude::Stringifier;
+use sophia::inmem::graph::LightGraph;
+use sophia::turtle::parser::{nt, turtle};
 use sophia::turtle::serializer::nt::NtSerializer;
 use sophia::turtle::serializer::turtle::{TurtleConfig, TurtleSerializer};
-use std::io::BufReader;
+use std::ffi::OsStr;
+use std::io::{BufReader, BufWriter};
+use std::path::PathBuf;
 //use std::io::{BufReader, stdin};
 
 /*enum Format {
@@ -35,21 +41,22 @@ struct Args {
 #[derive(Subcommand)]
 enum Command {
     Info {
-        hdt_input_file: String,
+        input_path: PathBuf,
     },
     Convert {
-        #[arg(short, long)]
-        /// export as RDF Turtle, default is N-Triples
-        turtle: bool,
+        // #[arg(short, long)]
+        // /// export as RDF Turtle, default is N-Triples
+        // turtle: bool,
         /*
         #[arg(short, long)]
         /// Count triples only, do not print them
         count: bool,
         */
         /// the HDT file to load from
-        hdt_input_file: String,
-        /// the RDF file to create, if not given it is written to stdout
-        rdf_output_file: Option<String>,
+        input_path: PathBuf,
+        // /// the RDF file to create, if not given it is written to stdout
+        // rdf_output_path: Option<String>,
+        output_path: PathBuf,
     },
 }
 
@@ -58,7 +65,7 @@ fn main() -> Result<(), Report> {
     //env_logger::init();
     //env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     let args = Args::parse();
-    /*let hdt = match args.hdt_input_file {
+    /*let hdt = match args.input_path {
         Some(filename) => {
             let file =
                 File::open(filename.clone()).wrap_err_with(|| format!("Error opening input file {}", filename))?;
@@ -73,42 +80,88 @@ fn main() -> Result<(), Report> {
         }
     };*/
     match args.command {
-        Command::Info { hdt_input_file } => {
-            let file = File::open(hdt_input_file.clone())
-                .with_context(|| format!("Error opening input HDT file {hdt_input_file}"))?;
+        Command::Info { input_path } => {
+            let file = File::open(input_path.clone())
+                .with_context(|| format!("Error opening input HDT file {input_path:?}"))?;
             let mut reader = BufReader::new(file);
-            ControlInfo::read(&mut reader)?;
-            let header = Header::read(&mut reader)?;
-            println!("HDT File: {:#?}", header.body);
+            match input_path.extension().and_then(OsStr::to_str) {
+                Some("nt") => {
+                    let triples = nt::parse_bufread(reader).collect_triples();
+                    let g: LightGraph = triples.unwrap();
+                    println!("RDF N-Triples File with ~{} triples", g.triples().size_hint().0);
+                }
+                Some("ttl") => {
+                    let triples = turtle::parse_bufread(reader).collect_triples();
+                    let g: LightGraph = triples.unwrap();
+                    println!("RDF Turtle with ~{} triples", g.triples().size_hint().0);
+                }
+                Some("hdt") => {
+                    ControlInfo::read(&mut reader)?;
+                    let header = Header::read(&mut reader)?;
+                    //println!("{}",ByteSize(hdt.size_in_bytes() as u64).to_string());
+                    println!("HDT File: {:#?}", header.body);
+                }
+                Some(x) => {
+                    eprintln!("Unknown RDF extension {x:?}, aborting.");
+                }
+                None => {
+                    println!("File has no extension, RDF format cannot be determined, aborting.");
+                }
+            }
         }
-        Command::Convert { hdt_input_file, rdf_output_file, turtle } => {
-            let file = File::open(hdt_input_file.clone())
-                .with_context(|| format!("Error opening input HDT file {hdt_input_file}"))?;
-            let hdt = Hdt::read(BufReader::new(file))
-                .with_context(|| format!("Error loading input HDT from {hdt_input_file}"))?;
-            let count = hdt.triples.len();
+        Command::Convert { input_path, output_path /* turtle*/ } => {
+            let file = File::open(input_path.clone())
+                .with_context(|| format!("Error opening input HDT file {input_path:?}"))?;
+            let reader = BufReader::new(file);
+
+            let hdt = match input_path.extension().and_then(OsStr::to_str) {
+                Some("hdt") => {
+                    Hdt::read(reader).with_context(|| format!("Error loading input HDT from {input_path:?}"))?
+                }
+                Some("nt") => Hdt::read_nt(&input_path)
+                    .with_context(|| format!("Error loading input N-Triples file from {input_path:?}"))?,
+                _ => {
+                    panic!(
+                        "Input file has unsupported or no extension, RDF format cannot be determined, aborting."
+                    );
+                }
+            };
+            // let count = hdt.triples.len();
             /*if args.count {
                 println!("Parsing returned {} triples", count);
                 return Ok(());
             }*/
-            let s = match turtle {
-                true => {
+            let output_file = File::create(&output_path)?;
+            let mut writer = BufWriter::new(output_file);
+            match output_path.extension().and_then(OsStr::to_str) {
+                Some("ttl") => {
                     let config = TurtleConfig::new().with_pretty(true);
                     //.with_own_prefix_map(prefixes().clone());
-                    TurtleSerializer::new_stringifier_with_config(config)
+                    //TurtleSerializer::new_stringifier_with_config(config)
+                    TurtleSerializer::new_with_config(writer, config)
                         .serialize_graph(&hdt)
-                        .wrap_err("error serializing graph as RDF Turtle")?
-                        .to_string()
+                        .wrap_err("error serializing graph as RDF Turtle")?;
+                    //.to_string()
                 }
-                false => {
+                Some("nt") => {
                     // Default: export the complete graph as N-Triples.
-                    NtSerializer::new_stringifier()
+                    //NtSerializer::new_stringifier()
+                    NtSerializer::new(writer)
                         .serialize_graph(&hdt)
-                        .wrap_err("error serializing graph as N-Triples")?
-                        .to_string()
+                        .wrap_err("error serializing graph as N-Triples")?;
+                    //.to_string()
+                }
+                Some("hdt") => {
+                    hdt.write(&mut writer)?;
+                }
+                _ => {
+                    panic!(
+                        "Output file has no extension or one signifying an unsupported export format, aborting."
+                    );
                 }
             };
-            println!("{s}");
+            println!("Successfully converted {input_path:?} to {output_path:?}");
+            // println!("{s}");
         }
     }
     Ok(())
